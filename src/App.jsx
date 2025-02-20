3caab8703276391e9be29f4af5d09231c21ef328
DorsaBeigifard <dorsabeigi@gmail.com>
2025-02-20 22:35:25 +0330

    created form

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,17 @@
 import "./App.css";
+import AddNewNote from "./components/AddNewNote";
+
+ 
 function App() {
-  return <h1>02: NoteApp starter file</h1>;
+  return (
+    <div className="container">
+      <div className="note-header"></div>
+      <div className="note-app">
+        <AddNewNote />
+        <div className="note-container"></div>
+      </div>
+    </div>
+  );
 }
 
 export default App;
---
+++ src/components/AddNewNote.jsx
@@ -0,0 +1,43 @@
+import { useState } from "react";
+
+export default function AddNewNote() {
+  const [title, setTitle] = useState("");
+  const [description, setDescription] = useState("");
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const newNote = {
+      title,
+      description,
+      id: Date.now(),
+      completed: false,
+      createdAt: new Date().toISOString(),
+    };
+    setTitle("");
+    setDescription("");
+  };
+  return (
+    <div className="add-new-note">
+      <h2>Add New Note</h2>
+      <form className="note-form" onSubmit={handleSubmit}>
+        {/* Controlled elements: */}
+        <input
+          value={title}
+          onChange={(e) => setTitle(e.target.value)}
+          type="text"
+          className="text-field"
+          placeholder="note title..."
+        />
+        <input
+          value={description}
+          onChange={(e) => setDescription(e.target.value)}
+          type="text"
+          className="text-field"
+          placeholder="note description..."
+        />
+        <button type="submit" className="btn btn--primary">
+          Add new note
+        </button>
+      </form>
+    </div>
+  );
+}
